f55f17f8e3a0f98ea5287865d66cc940076771d8
EventideX <844058637@qq.com>
2017-01-28 04:23:25 +0800

    second

--- PAT Advanced Level Practise/1002/A+B for Polynomials.cpp
+++ PAT Advanced Level Practise/1002/A+B for Polynomials.cpp
@@ -4,14 +4,16 @@
 using namespace std;
 int main()
 {
-	int i,j,n,m,s,t;
+	int i,j,n,m,s,t,b[1001];
 	float a[1001],k;
 	memset(a,0,sizeof(a));
+	memset(b,0,sizeof(b));
 	cin>>n;
 	for (i=0;i<n;i++)
 	{
 		cin>>j>>k;
 		a[j]=k;
+		b[j]=1;
 	}
 	t=n;
 	cin>>n;
@@ -20,12 +22,14 @@ int main()
 		cin>>j>>k;
 		if (a[j]==0) t++; 
 		a[j]+=k;
+		b[j]=1;
 	}
 	cout<<t<<' ';
+	n=t; 
 	t=0;
 	for (i=1000;i>=0;i--)
 	{
-		if (a[i]!=0)
+		if (b[i]!=0)
 		if (t<n)
 		{
 			printf("%d %.1f ",i,a[i]);
